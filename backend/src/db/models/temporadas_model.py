daaa0670f56fa7b8a2beb1f6b84c490501a9e711
ramiro009 <ramiro.bazzi@philips.edu.ar>
2026-05-15 09:36:44 -0300

    temporadas model

---
+++ backend/src/db/models/temporadas_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
+
+from src.db.connection import Base
+
+
+class Temporadas(Base):
+    __tablename__ = "temporadas"
+
+    id = Column(Integer, primary_key=True)
+    contenido_id = Column(Integer, ForeignKey("contenidos.id"), nullable=False)
+    numero = Column(Integer, nullable=False)
+    anio = Column(Integer)
