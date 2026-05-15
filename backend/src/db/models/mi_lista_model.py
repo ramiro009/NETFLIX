c19d674f09504b2840b234866a22c28e5e2ceecc
ramiro009 <ramiro.bazzi@philips.edu.ar>
2026-05-15 11:16:31 -0300

    tenía un eror en mi_lista_model jej

--- backend/src/db/models/mi_lista_model.py
+++ backend/src/db/models/mi_lista_model.py
@@ -7,6 +7,7 @@ from src.db.connection import Base
 class MiLista(Base):
     __tablename__ = "mi_lista"
 
+    id = Column(Integer, primary_key=True)
     perfil_id = Column(Integer, ForeignKey("perfiles.id"), nullable=False)
     contenido_id = Column(Integer, ForeignKey("contenidos.id"), nullable=False)
     fecha_agregada = Column(DateTime, server_default=func.now())
